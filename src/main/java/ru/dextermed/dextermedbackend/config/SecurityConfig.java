b356af6b0f9080cfcec37d9d591e1d1337e5ac19
agent_tim <agent_tim@mail.ru>
2023-12-08 13:25:18 +0500

    удаляем auth provider

--- src/main/java/ru/dextermed/dextermedbackend/config/SecurityConfig.java
+++ src/main/java/ru/dextermed/dextermedbackend/config/SecurityConfig.java
@@ -1,24 +1,32 @@
 package ru.dextermed.dextermedbackend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import ru.dextermed.dextermedbackend.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.dextermed.dextermedbackend.service.UserEntityDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final UserEntityDetailsService userEntityDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(UserEntityDetailsService userEntityDetailsService) {
+        this.userEntityDetailsService = userEntityDetailsService;
     }
 
     @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userEntityDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }

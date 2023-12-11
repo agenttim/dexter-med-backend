653f7226cf6ccc2c5b85a049d6865667f0494389
agent_tim <agent_tim@mail.ru>
2023-12-11 09:41:41 +0500

    в процессе внедрения авторизации

--- src/main/java/ru/dextermed/dextermedbackend/service/UserEntityDetailsService.java
+++ src/main/java/ru/dextermed/dextermedbackend/service/UserEntityDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.dextermed.dextermedbackend.model.UserEntity;
 import ru.dextermed.dextermedbackend.repository.UserEntityRepository;
 import ru.dextermed.dextermedbackend.security.UserEntityDetails;
@@ -21,7 +22,12 @@ public class UserEntityDetailsService implements UserDetailsService {
         this.userEntityRepository = userEntityRepository;
     }
 
+    public Optional<UserEntity> findByUserName(String username) {
+        return userEntityRepository.findByUsername(username);
+    }
+
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<UserEntity> userEntity = userEntityRepository.findByUsername(username);
 
@@ -30,4 +36,6 @@ public class UserEntityDetailsService implements UserDetailsService {
 
         return new UserEntityDetails(userEntity.get());
     }
+
+
 }

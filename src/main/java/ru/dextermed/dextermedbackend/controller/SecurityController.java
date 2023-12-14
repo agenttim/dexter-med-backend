dd98ea67ae724c63485e2001f3d72d587abf127c
agent_tim <agent_tim@mail.ru>
2023-12-14 14:30:30 +0500

    завершаю внедрение spring security + jwt

--- src/main/java/ru/dextermed/dextermedbackend/controller/SecurityController.java
+++ src/main/java/ru/dextermed/dextermedbackend/controller/SecurityController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.dextermed.dextermedbackend.security.UserEntityDetails;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/api")
 public class SecurityController {
@@ -16,5 +18,11 @@ public class SecurityController {
         UserEntityDetails userEntityDetails = (UserEntityDetails) authentication.getPrincipal();
         System.out.println(userEntityDetails.getUserEntity());
     }
+
+    @GetMapping("/showUserInfo2")
+    public String showUserInfo2(Principal principal) {
+        return principal.toString();
+    }
+
 
 }
